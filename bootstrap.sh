49efcd0958fd2e06689bcdfdb685c4a6fd7802d2
Diar Kourehpaz <99diagha@gmail.com>
2018-12-24 17:10:49 +0100

    Minor change to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,8 +11,18 @@ apt-add-repository ppa:ondrej/php
 apt-get update
 
 apt-get install -y php7.2
-
 apt-get install -y libapache2-mod-php7.2
+
+cat << EOF | sudo tee -a /etc/apache2/sites-available/default.conf
+<Directory "/var/www/html">
+    AllowOverride All
+</Directory>
+<VirtualHost *:80>
+    DocumentRoot /var/www/html
+    ServerName learnphp.test
+</VirtualHost>
+EOF
+a2ensite default.conf
 
 service apache2 restart
 
@@ -23,7 +33,6 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password password roo
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 
 apt-get install -y mysql-server
-
 apt-get install -y php7.2-mysql
 
 sudo service apache2 restart
